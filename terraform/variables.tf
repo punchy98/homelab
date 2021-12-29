f0b8a63e73322b831c951090dc50d4a19bf003ef
Michael Maldonado <12002902+punchy98@users.noreply.github.com>
2021-12-29 16:18:31 +0000

    variables and changes to main tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,20 @@
 variable "vsphere_server" {}
 variable "vsphere_user" {}
 variable "vsphere_password" {}
+# vsphere datacenter the virtual machine will be deployed to. empty by default.
+variable "vsphere_datacenter" {}
+
+# vsphere resource pool the virtual machine will be deployed to. empty by default.
+variable "vsphere_resource_pool" {}
+
+# vsphere datastore the virtual machine will be deployed to. empty by default.
+variable "vsphere_datastore" {}
+
+# vsphere network the virtual machine will be connected to. empty by default.
+variable "vsphere_network" {}
+
+# vsphere virtual machine template that the virtual machine will be cloned from. empty by default.
+variable "vsphere_virtual_machine_template" {}
+
+# the name of the vsphere virtual machine that is created. empty by default.
+variable "vsphere_virtual_machine_name" {}
