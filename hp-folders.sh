d7dc5ccda221347612e6d46cd68be57b8feed697
Michael Maldonado <michael@maldonado.tech>
2022-10-25 12:37:33 -0500

    moving lab

---
+++ hp-folders.sh
@@ -0,0 +1,8 @@
+\#!/bin/bash
+cd /mnt/media/movies/
+HP="Harry Potter and the"
+mv "${HP}Order of the Phoenix" "${HP} Order of the Phoenix"
+mv "${HP}Half-blood Prince" "${HP} Half-blood Prince"
+mv "${HP}Prisoner of Azkaban" "${HP} Prisoner of Azkaban"
+mv "${HP}Sorcerers Stone" "${HP} Sorcerers Stone"
+
